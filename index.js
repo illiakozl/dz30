1dc21372a3ecf0ecebc5e398cc7a56c9b188158e
illia kozlov <samsung.id.od@gmail.com>
2023-07-16 22:04:22 +0200

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+const num1 = +prompt("Please, enter first number");
+            const num2 = +prompt("Please, enter second number");
+            const message = (
+                num1 + " + " + num2 + " = " + (num1 + num2) + "\n" +
+                num2 + " - " + num2 + " = " + (num1 - num2) + "\n" +
+                num1 + " * " + num2 + " = " + (num1 * num2) + "\n" +
+                num1 + " / " + num2 + " = " + (num1 / num2)
+            );
